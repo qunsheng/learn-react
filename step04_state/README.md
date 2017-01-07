e1c6902f0f18dff5f598ebaa72fe93fdedd03ebf
qunny <qunny>
2017-01-07 16:11:54 -0500

    update

---
+++ step04_state/README.md
@@ -0,0 +1,10 @@
+# Step 04 State
+
+copy everything from step 02
+
+`npm install`
+
+`npm start`
+
+Modify index.js to: 1) implement getInitialState function to add todos object into component state; 
+2) implement component render function to use component state
---
+++ step04_state/src/app/index.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['wash up', 'eat some cheese', 'take a nap']
+        }
+    }, //getInitialState
+    render: function(){
+        return(
+            <div id="todo-list">
+                <p> The busiest people have the most leisure......</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
+
+            </div>
+
+        );
+    }
+});
+
+
+ReactDOM.render(<TodoComponent />, document.getElementById('component-wrapper'));
